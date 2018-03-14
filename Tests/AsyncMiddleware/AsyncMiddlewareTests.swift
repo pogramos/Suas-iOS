1b39905452390541e5b1550b7e870ba51fddfe66
divo <sdiviney@zendesk.com>
2018-03-14 14:25:57 +0000

    updating tests

--- Tests/AsyncMiddleware/AsyncMiddlewareTests.swift
+++ Tests/AsyncMiddleware/AsyncMiddlewareTests.swift
@@ -116,10 +116,5 @@ class DummyFileManager: FileManager {
   override func contents(atPath path: String) -> Data? {
     return dataToReturn
   }
-  
-  override func createFile(atPath path: String, contents data: Data?, attributes attr: [FileAttributeKey : Any]? = nil) -> Bool {
-    writtenPath = path
-    writtenData = data
-    return true
-  }
+    
 }
